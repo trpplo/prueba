571430161666befdd3cb5cc4ee3c6b765240882b
trpplo <plopez.o@gmv.com>
2024-06-11 14:32:55 +0200

    Add both files

---
+++ names.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std; 
+int main() {
+	char* name;
+	name = new char[30];
+	cout<<"Introduzca su nombre: "; cin.getline(name,30);
+	cout<<"Su nombre es: "<<name;
+
+    return 0;
+}
+
