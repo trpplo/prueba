e99e3ca299805d5a72f85c33306889c3c95aa12e
trpplo <plopez.o@gmv.com>
2024-06-12 11:17:31 +0200

    Add changes names

--- names.cpp
+++ names.cpp
@@ -6,7 +6,7 @@ int main() {
 	char* name;
 	name = new char[30];
 	cout<<"Introduzca su nombre: "; cin.getline(name,30);
-	cout<<"Su nombre es: "<<name;
+	cout<<"Su nombre es "<<name<< "\n";
 
     return 0;
 }
