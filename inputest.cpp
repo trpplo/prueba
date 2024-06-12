60d79f8a342b5417fe3e0023b4e1a8784ece9222
trpplo <plopez.o@gmv.com>
2024-06-12 11:19:43 +0200

    Setting input test

---
+++ inputest.cpp
@@ -0,0 +1,13 @@
+include <iostream>
+#include <fstream>
+using namespace std;
+void main(){
+int num;
+char tmp[num];
+char file[]="archivolectura.txt";
+ifstream file2(file);
+while(!file2.eof());
+File>>tmp;
+Cout<<tmp;
+
+
