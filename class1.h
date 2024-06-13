58bb1c52a10c79ac45f136ebb5ff0a8e0b556394
trpplo <plopez.o@gmv.com>
2024-06-13 09:07:35 +0200

    modified class1.h in branch1 -- added userpromocode

--- class1.h
+++ class1.h
@@ -4,8 +4,10 @@ class class1{
     public:
     int usercode;
     char* username;
-    class1(int usercode, char* username){
+    int userpromocode;
+    class1(int usercode, char* username, int userpromocode){
         this-> usercode=usercode;
         this-> username=username;
+        this -> userpromocode =userpromocode;
     };
 };
