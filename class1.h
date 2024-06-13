b16b4b75108918b8b012a2fa5bf64bf2f990b6db
trpplo <plopez.o@gmv.com>
2024-06-13 09:02:05 +0200

    Modified/Created class1.h in branch1

---
+++ class1.h
@@ -0,0 +1,11 @@
+#pragma once 
+#include<iostream>
+class class1{
+    public:
+    int usercode;
+    char* username;
+    class1(int usercode, char* username){
+        this-> usercode=usercode;
+        this-> username=username;
+    };
+};
